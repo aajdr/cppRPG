10e6a09bbae6613944cd3bb111b7f43903d147c4
Cache Makers <Image15@iMac-I.local>
2017-02-28 17:56:41 -0700

    Added hero

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 #define WIDE 800
 #define HIGH 600
@@ -7,11 +8,19 @@ int main()
 {
   sf::RenderWindow window(sf::VideoMode(WIDE,HIGH),"Adventure!");
 
-  sf::CircleShape circle(100);
+  sf::CircleShape circle(20);
   circle.setFillColor(sf::Color::White);
   circle.setPosition(100,100);
 
+  sf::RectangleShape hero(sf::Vector2f(10,10));
+  hero.setFillColor(sf::Color::Red);
+  hero.setPosition(400,300);
+
   int circleDirection = 0;
+
+  double heroSpeed = 1;
+
+  int points = 10;
 
   //Game Loop
   while(window.isOpen())
@@ -26,18 +35,45 @@ int main()
         window.close();
       }
     }
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && hero.getPosition().y > 0)
+    {
+      hero.move(0,-heroSpeed);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && hero.getPosition().y < HIGH-hero.getSize().x)
+    {
+      hero.move(0,heroSpeed);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && hero.getPosition().x < WIDE-hero.getSize().x)
+    {
+      hero.move(heroSpeed,-0);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && hero.getPosition().x > 0)
+    {
+      hero.move(-heroSpeed,0);
+    }
+
+    if(hero.getGlobalBounds().intersects(circle.getGlobalBounds()))
+    {
+      std::cout << "Touching!" << std::endl;
+    }
+    else
+    {
+      std::cout << "Not Touching!" << std::endl;
+    }
+
     window.clear(sf::Color::Black);
 
     //update stuff - This is Aaron
     if(circleDirection == 0)
     {
-      circle.move(0.1,0);
+      circle.move(0.5,0);
     }
     else
     {
-      circle.move(-0.1,0);
+      circle.move(-0.5,0);
     }
-    if(circle.getPosition().x > 600)
+    if(circle.getPosition().x > 760)
     {
       circleDirection = 1;
     }
@@ -47,6 +83,7 @@ int main()
     }
 
     //render - This is Max
+    window.draw(hero);
     window.draw(circle);
     window.display();
   }
